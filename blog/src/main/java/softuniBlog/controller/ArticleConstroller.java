a49338bb549300fc4ced3c24b252b5808d0b1b9e
drunin89 <drunin89@gmail.com>
2017-04-10 21:24:59 +0300

    Add Admin Functionality (delete and edit articles)

--- blog/src/main/java/softuniBlog/controller/ArticleConstroller.java
+++ blog/src/main/java/softuniBlog/controller/ArticleConstroller.java
@@ -2,6 +2,7 @@ package softuniBlog.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -47,15 +48,107 @@ public class ArticleConstroller {
 
     }
 
+
+    @GetMapping("/article/edit/{id}")
+    @PreAuthorize("isAuthenticated()")
+    public String edit(@PathVariable Integer id, Model model){
+        if(!this.articleRepository.exists(id)){
+            return "redirect:/";
+        }
+        Article article = this.articleRepository.findOne(id);
+
+        if(!this.isUserAuthorOrAdmin(article)){
+            return "redirect:/";
+        }
+
+        model.addAttribute("view", "article/edit");
+        model.addAttribute("article", article);
+
+        return "base-layout";
+    }
+
+
+
     @GetMapping("/article/{id}")
     public String details(Model model, @PathVariable Integer id) {
         if(!this.articleRepository.exists(id)){
             return "redirect:/";
         }
+
+        if(!(SecurityContextHolder.getContext().getAuthentication()
+                instanceof AnonymousAuthenticationToken)){
+            UserDetails principal = (UserDetails) SecurityContextHolder.getContext()
+                    .getAuthentication().getPrincipal();
+
+            User entityUser = this.userRepository.findByEmail(principal.getUsername());
+
+            model.addAttribute("user", entityUser);
+        }
+
         Article article = this.articleRepository.findOne(id);
         model.addAttribute("view", "article/details");
         model.addAttribute("article", article);
 
         return "base-layout";
+    }
+
+    @PostMapping("/article/edit/{id}")
+    @PreAuthorize("isAuthenticated()")
+    public String editProcess(@PathVariable Integer id, ArticleBindingModel articleBindingModel) {
+        if(!this.articleRepository.exists(id)){
+            return "redirect:/";
+        }
+        Article article = this.articleRepository.findOne(id);
+        if(!this.isUserAuthorOrAdmin(article)){
+            return "redirect:/";
+        }
+        article.setTitle(articleBindingModel.getTitle());
+        article.setContent(articleBindingModel.getContent());
+        this.articleRepository.saveAndFlush(article);
+        return "redirect:/article/" + article.getId();
+
+    }
+
+    @GetMapping("/article/delete/{id}")
+    @PreAuthorize("isAuthenticated()")
+    public String delete(@PathVariable Integer id, Model model){
+        if(!this.articleRepository.exists(id)){
+            return "redirect:/";
+        }
+
+        Article article = this.articleRepository.findOne(id);
+        if(!this.isUserAuthorOrAdmin(article)){
+            return "redirect:/";
+        }
+        model.addAttribute("article", article);
+        model.addAttribute("view", "article/delete");
+
+        return "base-layout";
+    }
+
+    @PostMapping("/article/delete/{id}")
+    @PreAuthorize("isAuthenticated()")
+    public String deleteProcess(@PathVariable Integer id) {
+        if(!this.articleRepository.exists(id)){
+            return "redirect:/";
+        }
+        Article article = this.articleRepository.findOne(id);
+        if(!this.isUserAuthorOrAdmin(article)){
+            return "redirect:/";
+        }
+        this.articleRepository.delete(article);
+
+        return "redirect:/";
+
+    }
+
+    private boolean isUserAuthorOrAdmin(Article article){
+        UserDetails user = (UserDetails) SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getPrincipal();
+
+        User userEntity = this.userRepository.findByEmail(user.getUsername());
+        return userEntity.isAdmin() || userEntity.isAuthor(article);
     }
 }
